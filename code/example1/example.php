97e596689401c64b4a5608beea0d8d925bab58b6
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-27 14:37:39 +0200

    working Example 1

--- code/example1/example.php
+++ code/example1/example.php
@@ -4,13 +4,14 @@
  * @file Create user.
  */
 
+include_once __DIR__ . '/vendor/autoload.php';
 
 $first = 'James';
 $last = 'Bond';
 
 if (strlen($first) >= 2) {
     if (strlen($last) >= 2) {
-        $r = php_bergen_create_user($first, $last);
+        $r = \Phpbergen\User::php_bergen_create_user($first, $last);
         print_r($r);
     }
 }
--- code/example1/src/Mysql.php
+++ code/example1/src/Mysql.php
@@ -4,13 +4,9 @@ namespace Phpbergen;
 
 class Mysql {
 
-    public function php_bergen_connect()
+    public static function php_bergen_connect()
     {
-        include_once 'setup.php';
-        global $host;
-        global $user;
-        global $pass;
-        global $db;
+        include_once __DIR__ . '/../setup.php';
         $db = mysqli_connect($host, $user, $pass, $db);
 
         return $db;
@@ -18,7 +14,7 @@ class Mysql {
 
     public static function php_bergen_insert_user($first, $last_name)
     {
-        $db = php_bergen_connect();
+        $db = self::php_bergen_connect();
         mysqli_query($db, "INSERT INTO user (first, lastname) VALUES ('$first' , '$last_name')");
         $id = mysqli_insert_id($db);
         $results = mysqli_query($db, "SELECT uid, first, lastname FROM user WHERE uid = $id");
