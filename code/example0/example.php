1f18d3a45bebc1e14d891b8a77a806a0d6eec309
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-27 13:10:33 +0200

    Uglify

--- code/example0/example.php
+++ code/example0/example.php
@@ -1,19 +1,14 @@
 <?php
-
 /**
  * @file Create user.
  */
-
 include  'inc/user.php';
-
-$first = 'James';
-$last = 'Bond';
-
-if (strlen($first) >= 2) {
-    if (strlen($last) >= 2) {
-        $r = php_bergen_create_user($first, $last);
-        print_r($r);
-    }
-}
+$first='James';
+$last='Bond';
+if(strlen($first) >= 2){
+  if(strlen($last) >= 2){
+  $r = php_bergen_create_user($first, $last);
+  print_r($r);
+}}
 
 ?>
