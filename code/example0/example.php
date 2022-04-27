ef9f3cedf21bda6b735fe337f75f7920a0b82ff6
Stein Magne Bjorklund <steinmb@smbjorklund.com>
2022-04-27 10:41:30 +0200

    example 0

---
+++ code/example0/example.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file Create user.
+ */
+
+include  'inc/user.php';
+
+$first = 'James';
+$last = 'Bond';
+
+if (strlen($first) >= 2) {
+    if (strlen($last) >= 2) {
+        $r = php_bergen_create_user($first, $last);
+        print_r($r);
+    }
+}
+
+?>
---
+++ code/example0/inc/user.php
@@ -0,0 +1,11 @@
+<?php
+
+include 'mysql.php';
+
+function php_bergen_create_user($first, $last) {
+    return php_bergen_insert_user($first, $last);
+}
+
+//function php_bergen_create_super_user($first, $last) {
+//    return php_bergen_insert_user($first, $last);
+//}
